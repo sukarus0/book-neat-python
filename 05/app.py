1974c1dcd2eded287e325c17ab0cceebcda362bf
sukarus0 <sukarus@gamil.com>
2022-05-20 07:50:16 +0000

    black app.py

--- 05/app.py
+++ 05/app.py
@@ -69,8 +69,8 @@ def follow():
 @app.route("/unfollow", methods=["POST"])
 def unfollow():
     payload = request.json
-    user_id = int(payload['id'])
-    user_id_to_unfollow = int(payload['unfollow'])
+    user_id = int(payload["id"])
+    user_id_to_unfollow = int(payload["unfollow"])
 
     if user_id not in app.users or user_id_to_unfollow not in app.users:
         return "No user", 400
@@ -84,13 +84,10 @@ def unfollow():
 @app.route("/timeline/<int:user_id>", methods=["GET"])
 def timeline(user_id):
     if user_id not in app.users:
-        return 'No user', 400
+        return "No user", 400
 
-    follow_list = app.users[user_id].get('follow', set())
+    follow_list = app.users[user_id].get("follow", set())
     follow_list.add(user_id)
-    timeline = [tweet for tweet in app.tweets if tweet['user_id'] in follow_list]
+    timeline = [tweet for tweet in app.tweets if tweet["user_id"] in follow_list]
 
-    return jsonify({
-        'user_id' : user_id,
-        'timeline' : timeline
-    })
+    return jsonify({"user_id": user_id, "timeline": timeline})
