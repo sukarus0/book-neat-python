3aad5ab16a948380b1c79b67b5931ec25b5fc846
sukarus0 <sukarus@gamil.com>
2022-06-04 03:35:48 +0000

    test db setting & ping endpoint test

--- 08/config.py
+++ 08/config.py
@@ -7,4 +7,15 @@ db = {
 }
 DB_URL = f"mysql+mysqlconnector://{db['user']}:{db['password']}@{db['host']}:{db['port']}/{db['database']}?charset=utf8"
 
+test_db = {
+    'user'      : 'root',
+    'password'  : '1111',
+    'host'      : 'localhost',
+    'port'      : 3306,
+    'database'  : 'miniter_test'
+}
+test_config = {
+        'DB_URL' : f"mysql+mysqlconnector://{test_db['user']}:{test_db['password']}@{test_db['host']}:{test_db['port']}/{test_db['database']}?charset=utf8"
+}
+
 JWT_SECRET_KEY        = 'SOME_SUPER_SECRET_KEY'
